8a34a045e18455e6a5eec97732ca62dbc20dcf72
gabikun <36935705+gabikun@users.noreply.github.com>
2018-03-22 22:55:06 +0100

    Update motdepasse.py
    validation mdp

--- html/motdepasse.py
+++ html/motdepasse.py
@@ -1,4 +1,4 @@
-﻿from tkinter import *
+from tkinter import *
 from tkinter.font import families, Font
 
 #! /usr/bin/env python
@@ -12,57 +12,49 @@ class Application:
         self.fen.title("c.c")
         self.ftComic = Font (family = 'Comic Sans MS', size = -50,\
                 underline = False, weight = "bold",slant="italic")
-        ftComic2 = Font (family = "Comic Sans MS", size = -20,\
+        self.ftComic2 = Font (family = "Comic Sans MS", size = -20,\
                 underline = False)
 
 
         # On rajoute ligne 2 colonne 2 une entrée (un input)
         self.entreeUn = Entry (self.fen, show='*')
         self.entreeUn.grid (row = 1, column = 2, padx = 10, pady = 10)
-        self.entreeUn.configure (fg="blue",font = ftComic2)
+        self.entreeUn.configure (bg ='bisque', fg='maroon',font = self.ftComic2)
         self.entreeUn.focus_set()
 
 
         # On rajoute ligne 6 colonne 1 un bouton Valider
         boutonValider = Button (self.fen, text=' Valider ',command=self.valider)
         boutonValider.grid (row = 2, column = 2, pady = 10)
-        boutonValider.configure (fg="black", font = ftComic2)
+        boutonValider.configure (fg="black", font = self.ftComic2)
 
 
         # On rajoute ligne 7 colonne 2 un bouton Quitter
         boutonQuitter = Button (self.fen, text=' Quitter ',command=self.fen.quit)
         boutonQuitter.grid (row = 3,  column = 3, padx = 5, pady = 5)
-        boutonQuitter.configure (fg="black", font = ftComic2)
+        boutonQuitter.configure (fg="black", font = self.ftComic2)
 
         # On se donne la possibiliter de valider avec la touche entrée
         self.fen.bind('<Return>', self.valider1)
 
-        # On rajoute ligne 2 colonne 1  "collé" à droite (est)
-        labelDeux = Label (self.fen, text="Année")
-        labelDeux.configure (bg="black",fg="white",font = ftComic2)
-        labelDeux.grid (row = 2, column = 1)
-
+        # On créer un label pour le mot de passe
+        self.resultatmdp = Label (self.fen, text="Trouvez la clé")
+        self.resultatmdp.configure(bg="black", fg="white", font = self.ftComic2)
+        self.resultatmdp.grid(row = 3, column = 2)
 
     def valider1 (self,evt) :
         self.valider()
 
-    def valider (self) :
-        mdp = int(self.entreeUn.get())
-        if mdp=="123":
-            x="bissextile.gif"
-        else:
-            x="nonbissextile.gif"
 
+    def valider (self):
+        mdp = str(self.entreeUn.get())
+        if mdp == 'baba':
+            print ("bon mdp")
+            self.resultatmdp.config (text="Bonne clé", fg="green")
 
-    def Verification(self):
-        if Motdepasse.get() == 'python27':
-            # le mot de passe est bon : on affiche une boîte de dialogue puis on ferme la fenêtre
-            showinfo('Résultat','Mot de passe correct.\nAu revoir !')
-            Mafenetre.destroy()
         else:
-            # le mot de passe est incorrect : on affiche une boîte de dialogue
-            showwarning('Résultat','Mot de passe incorrect. Veuillez recommencer !')
-            Motdepasse.set('')
+            print ("mauvais mdp")
+            self.resultatmdp.config (text="Mauvaise clé", fg="red")
 
 
 
